5ba663aebf3569b50338583cfff0817c4a0ae7f6
Dongho Gwak <wing.flower.k@gmail.com>
2023-11-30 16:55:17 +0900

    23_11_30 test 1-3

--- lib/23_11_30/cleric.dart
+++ lib/23_11_30/cleric.dart
@@ -4,4 +4,17 @@ class Cleric{
   final int maxHp = 50;
   int mp = 10;
   final int maxMp = 10;
+
+  void selfAid() {
+    if (hp == maxHp) {
+      print("현재 HP가 최대입니다.");
+    } else {
+      if (mp >= 5) {
+        mp -= 5;
+        hp = maxHp;
+      } else {
+        print("MP가 부족합니다.");
+      }
+    }
+  }
 }
