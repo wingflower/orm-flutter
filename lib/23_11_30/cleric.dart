a96175508f064f204b2cf6de152e275334736056
Dongho Gwak <wing.flower.k@gmail.com>
2023-11-30 17:11:00 +0900

    23_11_30 test 1-4

--- lib/23_11_30/cleric.dart
+++ lib/23_11_30/cleric.dart
@@ -1,9 +1,13 @@
+import 'dart:math';
+
 class Cleric{
   String name;
   int hp = 50;
   final int maxHp = 50;
   int mp = 10;
   final int maxMp = 10;
+
+  Cleric(this.name);
 
   void selfAid() {
     if (hp == maxHp) {
@@ -16,5 +20,20 @@ class Cleric{
         print("MP가 부족합니다.");
       }
     }
+  }
+
+  int pray(int sec) {
+    int recoverMp = 0;
+    if (mp == maxMp) {
+      print("현재 MP가 최대입니다.");
+      return recoverMp;
+    }
+    int bonusMp = Random().nextInt(3);
+    recoverMp = sec + bonusMp;
+    if (mp + recoverMp > maxMp) {
+      recoverMp = maxMp - mp;
+    }
+    mp += recoverMp;
+    return recoverMp;
   }
 }
