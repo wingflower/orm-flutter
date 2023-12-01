1f66dab77880e37fe2c486d788fb7c0bccdcc9ec
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-01 16:29:58 +0900

    23_12_01 test 2

--- lib/23_12_01/cleric_test.dart
+++ lib/23_12_01/cleric_test.dart
@@ -1,19 +1,24 @@
 import 'cleric.dart';
 
 void main() {
-  final Cleric cleric001 = Cleric("AAA", hp:40, mp:5);
-  final Cleric cleric002 = Cleric("BBB", hp:35);
+  final Cleric cleric001 = Cleric("AAA", hp: 40, mp: 5);
+  final Cleric cleric002 = Cleric("BBB", hp: 35);
   final Cleric cleric003 = Cleric("CCC");
 
-  print("$cleric001.name}");
-  print(cleric001.hp);
-  print(cleric001.mp);
+  print('${cleric001.name} ${cleric001.hp} ${cleric001.mp}');
+  cleric001.selfAid();
+  cleric001.pray(1);
+  print('${cleric001.name} ${cleric001.hp} ${cleric001.mp}');
+  print('');
 
-  print(cleric002.name);
-  print(cleric002.hp);
-  print(cleric003.mp);
+  print('${cleric002.name} ${cleric002.hp} ${cleric002.mp}');
+  cleric002.selfAid();
+  cleric002.pray(1);
+  print('${cleric002.name} ${cleric002.hp} ${cleric002.mp}');
+  print('');
 
-  print(cleric003.name);
-  print(cleric003.hp);
-  print(cleric003.mp);
+  print('${cleric003.name} ${cleric003.hp} ${cleric003.mp}');
+  cleric003.selfAid();
+  cleric003.pray(1);
+  print('${cleric003.name} ${cleric003.hp} ${cleric003.mp}');
 }
