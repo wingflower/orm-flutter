d061fcda58914c0b732086644f61ae85ecad1bb7
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-04 23:19:02 +0900

    23_12_04 test 2-1

---
+++ lib/23_12_04/list_set_map.dart
@@ -0,0 +1,12 @@
+/// 대한민국의 도시이름 모음 (순서 상관 없음)
+/// set
+Set<String> citiesSet = {};
+
+/// 10명 학생의 시험 점수
+///
+List<int> scoreList = [];
+Map<String, double> scoreMap = {};
+
+/// 대한민국의 도시별 인구수 (순서 상관 없음)
+///
+Map<String, int> populationOfCities = {};
