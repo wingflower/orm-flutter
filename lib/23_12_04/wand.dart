0d5953f924f65ac4256186ada25a90ace9a0b629
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-04 15:52:42 +0900

    23_12_04 test 10-2

--- lib/23_12_04/wand.dart
+++ lib/23_12_04/wand.dart
@@ -3,10 +3,20 @@ const wandPower = 10.0;
 
 class Wand {
   String name;
-  double power;
+  double _power;
+
+  double get power => _power;
+
+  set power(double value) {
+    if (value >= 0.5 && value <= 100) {
+      _power = value;
+    } else {
+      throw Exception('지팡이의 마력이 비정상입니다.');
+    }
+  }
 
   Wand({
-    required this.name,
-    required this.power,
-  });
+    this.name = wandName,
+    double power = wandPower,
+  }) : _power = power;
 }
--- lib/23_12_04/wizard.dart
+++ lib/23_12_04/wizard.dart
@@ -1,8 +1,45 @@
 import 'package:src/23_12_04/wand.dart';
 
+const int maxHp = 50;
+const int maxMp = 50;
+
 class Wizard {
-  String name;
+  String _name;
   int hp;
-  int mp;
-  Wand? wand;
+  int _mp;
+  Wand wand;
+
+  String get name => _name;
+
+  set name(String value) {
+    if (value.length < 3) {
+      throw Exception('Wizard의 이름은 3자 이상입니다.');
+    }
+    _name = value;
+  }
+
+  int get mp => _mp;
+
+  set mp(int value) {
+    // if (mp < 0 && mp > maxMp) {
+    if (value < 0) {
+      // _mp = 0;
+      // return;
+      throw Exception('Wizard의 MP는 0 이상이어야 합니다.');
+    }
+    if (value > maxMp) {
+      // _mp = maxMp;
+      // return;
+      throw Exception('Wizard의 MP가 최대치를 초과하였습니다.');
+    }
+    _mp = value;
+  }
+
+  Wizard({
+    this.hp = maxHp,
+    required this.wand,
+    required String name,
+    required int mp,
+  })  : _name = name,
+        _mp = mp;
 }
