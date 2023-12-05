a4e828b48ac82a8bd78c225edd1be4d75c6dd46d
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-05 17:33:21 +0900

    23_12_05 test 12-4

--- lib/23_12_05/tangible_asset.dart
+++ lib/23_12_05/tangible_asset.dart
@@ -4,6 +4,8 @@ import 'package:src/23_12_05/thing.dart';
 ///
 /// 자산관리 프로그램의 유형 자산
 ///
+/// 형태가 있기 때문에 Thing 을 implements하여 weight를 가질 수 있도록 한다.
+///
 
 class TangibleAsset extends Asset implements Thing {
   String name;
