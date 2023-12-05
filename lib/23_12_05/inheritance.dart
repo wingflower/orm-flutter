a4dfe1d1fff46c642d0e1448324ff166c584ba2c
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-05 14:55:16 +0900

    23_12_05 test 11-2

--- lib/23_12_05/inheritance.dart
+++ lib/23_12_05/inheritance.dart
@@ -1,3 +1,4 @@
+// 잘못된 상속 출력
 class WrongInheritance {
   final String Person = 'Student';
   final String Car = 'Engine';
@@ -5,6 +6,26 @@ class WrongInheritance {
   final String Food = 'Susi';
   final String SuperMan = 'Man';
 
-  String get answer =>
-      'Car: $Car, Father: $Father, SuperMan: $SuperMan';
+  String get answer => 'Car: $Car, Father: $Father, SuperMan: $SuperMan';
 }
+
+// 통신 수단
+class NetworkDevice {}
+
+class Phone extends NetworkDevice {}
+
+class SmartPhone extends Phone {}
+
+// 이동 수단
+class Transportation {}
+
+class Car extends Transportation {}
+
+class SUV extends Car {}
+
+// 라면받침....
+class RamenHolder {}
+
+class Dictionary extends RamenHolder {}
+
+class YearBook extends Dictionary {}
