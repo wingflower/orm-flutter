6504cf3f6e4b37fe3e10d34df5833754ebb2367a
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-08 16:31:47 +0900

    23_12_08 test 15

--- lib/23_12_08/strong_box.dart
+++ lib/23_12_08/strong_box.dart
@@ -1,11 +1,64 @@
 class StrongBox<E> {
   E? _data;
+  KeyType keyType;
+  int tryCount = 0;
+
+  StrongBox({
+    required this.keyType,
+  });
 
   void put(E data) {
     _data = data;
   }
 
   Object? get() {
-    return _data;
+    tryCount += 1;
+    switch (keyType) {
+      case KeyType.padlock:
+        if (tryCount == 1024) {
+          return _data;
+        }
+      case KeyType.button:
+        if (tryCount == 10000) {
+          return _data;
+        }
+      case KeyType.dial:
+        if (tryCount == 30000) {
+          return _data;
+        }
+      case KeyType.finger:
+        if (tryCount == 1000000) {
+          return _data;
+        }
+    }
+    return null;
+  }
+}
+
+enum KeyType {
+  padlock,
+  button,
+  dial,
+  finger,
+}
+
+void main() {
+  String treasure = 'treasure';
+  for (KeyType key in KeyType.values) {
+    StrongBox strongBox = StrongBox(keyType: key);
+    strongBox.put(treasure);
+    int i = 0;
+    int nullCount = 0;
+    print('--- 금고를 털어보자 ---');
+    while (i <= 1000000) {
+      var tempSomething = strongBox.get();
+      if (tempSomething == null) {
+        nullCount += 1;
+      } else {
+        print('금고의 내용물은 $tempSomething 입니다.');
+        print('금고를 털려는 시도는 $nullCount 번 입니다.');
+      }
+      i += 1;
+    }
   }
 }
