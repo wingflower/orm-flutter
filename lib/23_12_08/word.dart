97f181b59ba235a8de057b975d577fd376c536f4
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-09 20:22:28 +0900

    23_12_08 test 16

--- lib/23_12_08/word.dart
+++ lib/23_12_08/word.dart
@@ -1,5 +1,6 @@
 ///
 /// i번째 글자가 모음인지 알려주는 isVowel() 함수 구현
+/// i번째 글자가 자음인지 알려주는 isConsonant() 함수 구현
 ///
 
 class Word {
@@ -12,5 +13,12 @@ class Word {
     }
     return false;
     // return word.substring(1, i + 1) == 'a';
+  }
+
+  bool isConsonant(int i) {
+    if (_vowelList.contains(word[i])) {
+      return false;
+    }
+    return true;
   }
 }
