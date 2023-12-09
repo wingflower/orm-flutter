33d5d49489a97788e297826cdc5efa416f3f8b5e
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-09 20:21:58 +0900

    23_12_08 test 16

--- lib/23_12_08/word.dart
+++ lib/23_12_08/word.dart
@@ -1,7 +1,16 @@
+///
+/// i번째 글자가 모음인지 알려주는 isVowel() 함수 구현
+///
+
 class Word {
+  final Set<String> _vowelList = {'a', 'e', 'i', 'o', 'u'};
   String word = '';
 
   bool isVowel(int i) {
+    if (_vowelList.contains(word[i])) {
+      return true;
+    }
+    return false;
     // return word.substring(1, i + 1) == 'a';
   }
 }
