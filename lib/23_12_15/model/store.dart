be0f819ba52e5cb58611dbe5a489fdb9a39a53c3
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-18 21:43:21 +0900

    23_12_15 test 23

--- lib/23_12_15/model/store.dart
+++ lib/23_12_15/model/store.dart
@@ -7,14 +7,21 @@ class Store {
   String address;
   String name;
   String stock;
+  int type;
 
   Store({
     required this.address,
     required this.name,
     required this.stock,
+    required this.type,
   });
 
   factory Store.fromJson(Map<String, dynamic> json) => _$StoreFromJson(json);
 
   Map<String, dynamic> toJson() => _$StoreToJson(this);
+
+  @override
+  String toString() {
+    return 'Store{name: $name, address: $address, stock: $stock, type: $type}';
+  }
 }
